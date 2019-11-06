50b183a48ecc5bbc2afc5e9417933cabc549a299
owori.juma@tracom.co.ke <tracom1234>
2019-11-06 18:00:17 +0300

    ChassisUUID upgrade

--- src/main/java/ke/axle/chassis/annotations/ChasisUUID.java
+++ src/main/java/ke/axle/chassis/annotations/ChasisUUID.java
@@ -9,9 +9,10 @@ import java.lang.annotation.Target;
  * Used to edit enable edit on child entities record alone
  *
  * @author Owori Juma
- * @version 1.2.3
+ * @version 1.3.1
  */
-@Target(value = {ElementType.TYPE})
+@Target(value = {ElementType.METHOD, ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ChasisUUID {
+    String name() default "";
 }
